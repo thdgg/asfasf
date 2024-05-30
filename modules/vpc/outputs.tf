faf8c5c7615db18e2dfeffd2806326fc4540e5e7
thdg <92166170+thdgg@users.noreply.github.com>
2024-05-30 10:06:33 +0700

    dsdg

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -7,8 +7,3 @@ output "subnet_id" {
   description = "The ID of the subnet"
   value       = aws_subnet.example.id
 }
-
-output "subnet_id" {
-  description = "Subnet ID"
-  value       = aws_subnet.example_1.id
-}
